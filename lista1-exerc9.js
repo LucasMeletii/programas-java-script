159f7866467314f5d2baa408430bfd7f4c2fc543
LucasMeletii <lucasmeleti2804@gmail.com>
2020-08-26 21:56:17 -0300

    finalizado a soma de divisores

--- lista1-exerc9.js
+++ lista1-exerc9.js
@@ -5,6 +5,12 @@ function chefe(){
     exerc9(numero)
 }
 
-function exerc9(num){
-    
+function exerc9(num){//supondo num =6
+    var soma = 0
+    for(var divisor=num; divisor >= 1; divisor--){//divisor começa em 6 e vai ate 1
+        if (num % divisor ==0){
+            soma = soma + divisor
+        }
+    }
+    console.log(`a soma do divisor de ${num} é ${soma}`)
 }
