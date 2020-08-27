f105b22ba321dd9242d57511cb757e546f0cca91
LucasMeletii <lucasmeleti2804@gmail.com>
2020-08-26 21:41:24 -0300

    exer7 calculando media

--- lista1-exer7.js
+++ lista1-exer7.js
@@ -1,1 +1,29 @@
-console.log(`exercicio 7`)
+chefe()
+
+function chefe(){
+    var vetor = []//declara vetor
+    exer7(vetor)//estamos passando o vetor como parametro - escopo global
+    calculaMedia(vetor)
+
+}
+
+function exer7(vet){//vet representa vetor -> o que fizemos com vet, será feito em vetor   
+
+    var numero = Number(prompt(`informe um numero`))
+    do{
+        
+        //coloca o numero em vet
+        vet.push(numero)
+        numero = Number(prompt(`informe outro numero, informe negativo para encerrar`))
+
+    }
+    while(numero >= 0)
+
+}
+function calculaMedia(){
+    var soma =0
+    for(var i=0; i < vet.length; i++){
+        soma = soma +vet[i]
+    }
+    console.log(`a média é ${soma/vet.length}`)
+}
